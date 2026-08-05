7d2c87c741928e609471aab6ad6a8a3433806ccd
Akash Kushwaha <its.akash.k7217@gmail.com>
2026-08-05 10:57:06 +0000

    type of data

---
+++ basic/2nd_DataType.js
@@ -0,0 +1,15 @@
+let a = "Akash";
+let b = true;
+let c = 2;
+let d ;
+null;
+console.log(a);
+console.log(typeof a);
+console.log(b);
+console.log(typeof b);
+console.log(c);
+console.log(typeof c);
+console.log(d);
+console.log(typeof d);
+console.log(null);
+console.log(typeof null);
