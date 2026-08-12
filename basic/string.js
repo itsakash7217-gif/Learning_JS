29835d73bf3c6aba5ebeff2262352393e26424a1
Akash Kushwaha <its.akash.k7217@gmail.com>
2026-08-12 07:15:41 +0000

    add on more fucntions of string

--- basic/string.js
+++ basic/string.js
@@ -15,6 +15,7 @@ let a =  "    Krishn kant want to go out this class  ";
 console.log(a);
 console.log(a.trim());
 console.log(a.trim().split(" "));
+console.log(a.trim().split(""));
 
 console.log(a.endsWith("class"));
 console.log(a.startsWith("    Krishn"));
@@ -27,3 +28,10 @@ console.log(a.toLowerCase());
 
 console.log(a.slice(0,10));
 console.log(a.slice(8));
+let b =765;
+console.log(typeof b.toString());
+let c =new String('87648');
+console.log(typeof c.valueOf());
+console.log(c.valueOf());
+console.log(c);
+console.log(c.bold());
