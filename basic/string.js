40bf9f7fbf339c659de38db43afae6226447ba96
Akash Kushwaha <its.akash.k7217@gmail.com>
2026-08-12 07:06:22 +0000

    Understanding of String

---
+++ basic/string.js
@@ -0,0 +1,29 @@
+// let name ="Akash";
+// let lastname ="Kushwaha";
+// console.log(name + " " + lastname);
+
+// console.log(`this is my namw ${name} and my lastname is ${lastname}`);
+
+let game = new String("coc");
+console.log(game);
+console.log(game.length);
+console.log(game[0]);
+console.log(game.substring(0,2));
+console.log(game.charAt(0));
+console.log(game.indexOf('c'));
+let a =  "    Krishn kant want to go out this class  ";
+console.log(a);
+console.log(a.trim());
+console.log(a.trim().split(" "));
+
+console.log(a.endsWith("class"));
+console.log(a.startsWith("    Krishn"));
+console.log(a.includes("want"));
+
+console.log(a.replace("Krishn","Akash"));
+
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+
+console.log(a.slice(0,10));
+console.log(a.slice(8));
