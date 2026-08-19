b706c39cce8b057070e1f89fcf42bd3f0216c7c7
Akash Kushwaha <its.akash.k7217@gmail.com>
2026-08-19 10:11:12 +0000

    add new learning related to arrays and date

---
+++ basic/array2.js
@@ -0,0 +1,27 @@
+// let a = ["spiderman","ironman","captain"];
+// let b =["black_adam","superman","batman"];
+
+// a.push(b);
+// console.log(a)  this will add in same array
+// console.log(a[3][1]);
+
+// let newarray = a.concat(b); new array is create and all value is spread in array withou any 2d formation
+// console.log(newarray);
+
+// let sp=[...a,...b]; // spread operator that spread all values
+// console.log(sp);
+
+// let a = [[2,4,5],[7,8,4,3],[5,4,[3,6,3]]];
+// let abc = a.flat(Infinity);
+// console.log(abc);
+
+
+// console.log(Array.isArray("Akash")) 
+// console.log(Array.from("Akash")) 
+
+
+let a = 100;
+let b =10000; 
+let c = 90;
+console.log(Array.of(a,b,c))
+
---
+++ basic/date.js
@@ -0,0 +1,6 @@
+let date = new Date();
+console.log(date.toString());
+
+let mydate = new Date("01-23-2026");
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
