8a6f0a5464680c4f6f89c7175a1aa17c67db679e
Akash Kushwaha <its.akash.k7217@gmail.com>
2026-08-20 16:28:38 +0000

    understanding of the destructuring

---
+++ basic/destructure.js
@@ -0,0 +1,9 @@
+const obj = {
+    name : "Akash",
+    age : 12,
+    height : 22
+
+}
+const {name : a} = obj;
+console.log(a);
+// destructure is used to change the name of the key of the object or call variable 
