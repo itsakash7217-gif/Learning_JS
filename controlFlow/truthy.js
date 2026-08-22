03c412180eacf87a82968ef1cfd2c49bebe07724
Akash Kushwaha <its.akash.k7217@gmail.com>
2026-08-22 05:36:27 +0000

    controll flow algo

---
+++ controlFlow/truthy.js
@@ -0,0 +1,30 @@
+// const email={};
+// if(Object.keys(email).length>0){
+//     console.log("true value");
+// }else{
+//     console.log("not varifies");
+// }
+
+
+// const email=[];
+// if(email.length>0){
+//     console.log("true value");
+// }else{
+//     console.log("not varifies");
+// }
+
+
+// const email="dfghj";
+// if(email){
+//     console.log("true value");
+// }else{
+//     console.log("not varifies");
+// }
+
+const email='';
+if(email){
+    console.log("true value");
+}else{
+    console.log("not varifies");
+}
+
